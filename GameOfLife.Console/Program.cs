a1ad62f9b0aa9a864789a8d74893a14cbbf14580
Adraca Asheart <Adraca@users.noreply.github.com>
2020-11-13 18:11:21 +0100

    feat: added Console example

---
+++ GameOfLife.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GameOfLife.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Game of Life!");
+            var runner = new Runner(2);
+            System.Console.Read();
+        }
+    }
+}
